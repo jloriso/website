ddc6907d9579258e15e4f0d446f759a1b4c8256f
jloriso <105647279+jloriso@users.noreply.github.com>
2024-01-09 18:56:46 -0600

    fixed dots

--- script.js
+++ script.js
@@ -10,12 +10,41 @@ function togglePicture(){
 
     let currImg = img.getAttribute("src");
 
-    console.log(currImg);
-
     if (currImg == "./assets/profile-pic-2.png"){
         img.setAttribute("src", "./assets/about-pic.png");
     }
     else{
         img.setAttribute("src", "./assets/profile-pic-2.png");
+    }
+
+    const dot1 = document.querySelector(".dot1");
+    const dot2 = document.querySelector(".dot2");
+    dot1.classList.toggle("active");
+    dot2.classList.toggle("active");
+}
+
+function currentSlide(slide){
+    let img = document.querySelector(".about-pic");
+
+    if (slide == 1){
+        img.setAttribute("src", "./assets/about-pic.png");
+        const dot1 = document.querySelector(".dot1");
+        const dot2 = document.querySelector(".dot2");
+        dot1.classList.toggle("active");
+        dot2.classList.toggle("active");
+    }
+    else if (slide == 2){
+        img.setAttribute("src", "./assets/profile-pic-2.png");
+        const dot1 = document.querySelector(".dot1");
+        const dot2 = document.querySelector(".dot2");
+        dot1.classList.toggle("active");
+        dot2.classList.toggle("active");
+    }
+    else{
+        img.setAttribute("src", "./assets/about-pic.png");
+        const dot1 = document.querySelector(".dot1");
+        const dot2 = document.querySelector(".dot2");
+        dot1.classList.toggle("active");
+        dot2.classList.toggle("active");
     }
 }

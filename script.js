b01199a75ae666d7899a28a9504b8de5ab96fc4b
Loriso, Joe <jloriso@walshgroup.com>
2024-07-05 13:28:30 -0500

    image buttons and logic

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
+function imageSlider(direction){
+    let img = document.querySelector(".about-pic");
+
+    let currImg = img.getAttribute("src");
+
+    if(direction == "right"){
+        toggleRight(img, currImg);
+    } else {
+        toggleLeft(img, currImg);
+    }
+}
+
+function toggleRight(img, currImg){
+    if (currImg == "./assets/seniorPic.jpg"){
+        img.setAttribute("src", "./assets/graduationSpeech.png");
+    } else if(currImg == "./assets/graduationSpeech.png"){
+        img.setAttribute("src", "./assets/ONUrun.jpg");
+    } else if(currImg == "./assets/ONUrun.jpg"){
+        img.setAttribute("src", "./assets/trackHeadshot.jpg");
+    } else if(currImg == "./assets/trackHeadshot.jpg"){
+        img.setAttribute("src", "./assets/seniorPic.jpg");
+    }
+}
+
+function toggleLeft(img, currImg){
+    if(currImg == "./assets/seniorPic.jpg"){
+        img.setAttribute("src", "./assets/trackHeadshot.jpg");
+    } else if(currImg == "./assets/trackHeadshot.jpg"){
+        img.setAttribute("src", "./assets/ONUrun.jpg");
+    } else if(currImg == "./assets/ONUrun.jpg"){
+        img.setAttribute("src", "./assets/graduationSpeech.png");
+    } else if(currImg == "./assets/graduationSpeech.png"){
+        img.setAttribute("src", "./assets/seniorPic.jpg");
+    }
+}
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
-
-function toggleDots(){
-    const dot1 = document.querySelector(".dot1");
-    const dot2 = document.querySelector(".dot2");
-    dot1.classList.toggle("active");
-    dot2.classList.toggle("active");
-}
-
-function togglePicture(){
-    let img = document.querySelector(".about-pic");
-
-    let currImg = img.getAttribute("src");
-
-    if (currImg == "./assets/profile-pic-2.png"){
-        img.setAttribute("src", "./assets/about-pic.png");
-    }
-    else{
-        img.setAttribute("src", "./assets/profile-pic-2.png");
-    }
-
-    toggleDots();
-}
-
-function currentSlide(slide){
-    let img = document.querySelector(".about-pic");
-
-    if (slide == 1){
-        img.setAttribute("src", "./assets/about-pic.png");
-        
-        toggleDots();
-    }
-    else if (slide == 2){
-        img.setAttribute("src", "./assets/profile-pic-2.png");
-        
-        toggleDots();
-    }
-    else{
-        img.setAttribute("src", "./assets/about-pic.png");
-        
-        toggleDots();
-    }
 }

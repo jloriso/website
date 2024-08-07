90027b8e948c13024b26c27fc629d3add409516d
Loriso, Joe <jloriso@walshgroup.com>
2024-08-07 13:31:14 -0500

    projects page

--- script.js
+++ script.js
@@ -58,3 +58,93 @@ function loadHeaderAndFooter(){
 }
 
 window.onload = loadHeaderAndFooter;
+
+let activeProject = 1;
+
+const dot1 = document.querySelector(".dot1");
+const dot2 = document.querySelector(".dot2");
+const dot3 = document.querySelector(".dot3");
+
+function projectSlider(direction){
+    if(direction == 'home'){
+        loadProjectOne();
+    } else if (direction == 'right'){
+        loadProjectRight();
+    } else if(direction == 'left'){
+        loadProjectLeft();
+    }
+}
+
+function loadProjectRight(){
+    if(activeProject == 1){
+        loadProjectTwo();
+        activeProject = 2;
+    } else if(activeProject == 2){
+        loadProjectThree();
+        activeProject = 3;
+    } else{
+        loadProjectOne();
+        activeProject = 1;
+    }
+}
+
+function loadProjectLeft(){
+    if(activeProject == 1){
+        loadProjectThree();
+        activeProject = 3;
+    } else if(activeProject == 2){
+        loadProjectOne();
+        activeProject = 1;
+    } else{
+        loadProjectTwo();
+        activeProject = 2;
+    }
+}
+
+function loadProjectOne(){
+    fetch("../projectInjections/projectOneInjection.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("project-info-injection-placeholder").innerHTML = data;
+        });
+
+        projectOneDots();
+}
+
+function projectOneDots(){
+    dot1.classList.add("active");
+    dot2.classList.remove("active");
+    dot3.classList.remove("active");
+}
+
+function loadProjectTwo(){
+    fetch("../projectInjections/projectTwoInjection.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("project-info-injection-placeholder").innerHTML = data;
+        });
+
+        projectTwoDots();
+}
+
+function projectTwoDots(){
+    dot1.classList.remove("active");
+    dot2.classList.add("active");
+    dot3.classList.remove("active");
+}
+
+function loadProjectThree(){
+    fetch("../projectInjections/projectThreeInjection.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("project-info-injection-placeholder").innerHTML = data;
+        });
+
+        projectThreeDots();
+}
+
+function projectThreeDots(){
+    dot1.classList.remove("active");
+    dot2.classList.remove("active");
+    dot3.classList.add("active");
+}
